782771361879dcd88af75985145f0064e9e84da5
sawanoboly <sawanoboriyu@higanworks.com>
2018-11-06 17:52:28 +0900

    bump 1.0.1

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: Shifter tips
 Plugin URI: https://github.com/getshifter/shifter-wp-tips
 Description: Show tips or help content about Shifter in your wp-admin
-Version: 1.0.0
+Version: 1.0.1
 Author: DigitalCube
 Author URI: https://getshifter.io
 License: GPL2
