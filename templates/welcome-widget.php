30d4bbfe834b78dd16b4c35a9cfbe72c2e50b6df
Tomohyco Tsunoda <tsunoda@digitalcube.jp>
2018-11-06 13:17:09 +0900

    :STATIC-1428: add title, URL, leads of support documents

--- templates/welcome-widget.php
+++ templates/welcome-widget.php
@@ -1,7 +1,16 @@
-<p>This is example content for dashboard widget</p>
-<p><b>We can update the widget content by the file</b></p>
-<ul>
-  <li>Lists</li>
-  <li>Lists</li>
-  <li>Lists</li>
-</ul>
+<h3>Essential support documnets for Shifter users</h3>
+<dl>
+	<dt><a href="https://support.getshifter.io/creating-sites-on-shifter/creating-a-new-wordpress-site">Creating a new WordPress site</a></dt>
+	<dd>Create new WordPress sites right from the Shifter Dashboard in just a few clicks.</dd>
+	<dt><a href="https://support.getshifter.io/migrating-to-shifter">Migrating to Shifter</a></dt>
+	<dd>Migrate an existing WordPress site in just a few clicks.</dd>
+	<dt><a href="https://support.getshifter.io/selectively-control-static-content-with-shifter-generator-settings">Selectively control static content with Shifter Generator Settings</a></dt>
+	<dd>Selectively control what content is created during the generating process and skip pages you don't need for a faster build time.</dd>
+	<dt><a href="https://support.getshifter.io/redirects-on-shifter/domain-redirects/wordpress-redirects-on-shifter-with-the-redirection-plugin">WordPress redirects on Shifter with the Redirection Plugin</a></dt>
+	<dd>Shifter supports redirect with Redirection plugin.</dd>
+	<dt><a href="https://support.getshifter.io/setting-up-custom-domains/custom-domains-on-shifter">Custom Domains on Shifter</a></dt>
+	<dd>How to add and assign custom domain to your Shifter site.</dd>
+  	<dt><a href="https://support.getshifter.io/setting-up-custom-domains/troubleshooting/troubleshooting-domain-validations">Troubleshooting domain validations</a></dt>
+  	<dd>If you've followed those steps and are still having trouble validating that domain here are a few tips to check your setup.</dd>
+  </dl>
+  <p>We have tonns of helpful documents on <a href="http://support.getshifter.io/">Shifter Documentation &mdash; Advice and answers from the Shifter Team</a>.
